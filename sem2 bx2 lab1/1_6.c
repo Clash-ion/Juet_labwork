22baf565a6a581cad1bba41eff387befd34db00c
Clash-ion <mukul0000kumar@gmail.com>
2021-05-06 15:02:15 +0530

    miscellaneous changes

--- sem2 bx2 lab1/1_6.c
+++ sem2 bx2 lab1/1_6.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 int main()
 {
-	// concat krega ye program agar string_1 ki length + string_2 ki length string_1 ki array ke length se kam ho
-
-	char string1[50], string2[50], i, j;
-	scanf("%s", string1);
-	scanf("%s", string2);
-	for (i = 0; *(string1 + i) != '\0'; ++i)
-		;										  //ye wala string1 ka length calculate krega bs
-	for (j = 0; *(string2 + j) != '\0'; ++j, ++i) // ye wala loop concat krega
+	char string1[100], string2[100];
+	int i, j;
+	scanf("%[^\n]%*c", string1);
+	scanf("%[^\n]%*c", string2);
+	for (i = 0; *(string1 + i) != '\0'; ++i);
+	for (j = 0; *(string2 + j) != '\0'; ++j, ++i)
 	{
 		*(string1 + i) = *(string2 + j);
 	}
-	*(string1 + i) = '\0'; //ye string1 = string1+string2 krne ke baad jo string1 ka last element hai uske end me null character add krdega jisse ki ek proper string bn jai
+	*(string1 + i) = '\0'; 
 	printf("%s", string1);
 	return 0;
 }
--- sem2 bx2 lab1/1_8.c
+++ sem2 bx2 lab1/1_8.c
@@ -1,24 +1,45 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
 	int n;
-	scanf("%d",&n);
-	int array[n],k;
-	for(int i= 0;i<n;i++)
-		scanf("%d",&array[i]);
-	for ( int i=0;i<n;i++)
-		for (int j = i+1;j<n;j++)
+	scanf("%d", &n);
+	int array[n], k;
+	for (int i = 0; i < n; i++)
+	scanf("%d", &array[i]);
+	scanf("%d", &k);
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = i + 1; j < n; j++)
 		{
-			if(array[j]<array[i])
+			if (array[j] < array[i])
 			{
-			int c= array[j];
-			array[j]=array[i];
-			array[i]= c;
+				int c = array[j];
+				array[j] = array[i];
+				array[i] = c;
 			}
 		}
+	}
 
-	scanf("%d",&k);
+	for (int i = n - k - 1; i < n; i++)
+	{
+		for (int j = k - 1 ; j < n ; j++ )
+		{
+			if (array[i] < array[j])
+			{
+				int c = array[i];
+				array[i] = array[j];
+				array[j] = c;
+			}
+		}
+	}
 	
-	printf("%d  smallest from start is %d\n%d largest from end is %d",k,array[k-1],n-k,array[n-k]);
-	
+		if (k==1)
+			printf("%dst smallest number is %d and %dst largest number is %d",k,array[k-1],k,array[n-k]);
+		else if (k==2)
+			printf("%dnd smallest number is %d and %dnd largest number is %d",k,array[k-1],k,array[n-k]);
+		else if (k==3)
+			printf("%drd smallest number is %d and %drd largest number is %d",k,array[k-1],k,array[n-k]);
+		else
+			printf("%dth smallest number is %d and %dth largest number is %d",k,array[k-1],k,array[n-k]);
+return 0; 
 }
